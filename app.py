38c2e056a3af3aa015f710eacf07bd35f00e9f59
mmcgreal <mmcgreal14@yahoo.com>
2016-01-25 12:45:00 -0500

    done

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import user
 import json
 import urllib2
 
+
 google_books_API = "https://www.googleapis.com/books/v1/volumes?q=%s&langRestrict=english&maxResults=40&orderBy=relevance&printType=books&showPreorders=true&key=AIzaSyCD5VLpef_d-PeOW7ISL3VsHQCdiuD_T1U"
+
 
 def apiCall(n):
     request = urllib2.urlopen(n)
